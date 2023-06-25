193036ba0de5a701c221d79f36aeca3b905c164f
Ammar Danish <ammardanishedu@gmail.com>
2023-06-25 10:03:44 +0800

    Added env variables

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
-    let posts = (await fetch('http://localhost:80/wordpress/wp-json/wp/v2/posts?_embed'));
+    let posts = (await fetch(import.meta.env.VITE_API + '/wp-json/wp/v2/posts?_embed'));
     let res = await posts.json()
     return {
         res
